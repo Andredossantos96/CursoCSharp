cf6b066711141a6003ec27ab7c4581c28c8369be
Andredossantos96 <andresantos216@gmail.com>
2021-12-05 14:18:26 -0300

    Métodos & Funções

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -6,6 +6,7 @@ using CursoCSharp.EstruturasDeControle;
 using CursoCSharp.ClassesEMetodos;
 using CursoCSharp.Colecoes;
 using CursoCSharp.OO;
+using CursoCSharp.MetodosEFuncoes;
 
 namespace CursoCSharp {
     class Program {
@@ -78,6 +79,14 @@ namespace CursoCSharp {
                 {"Abstract - OO", Abstract.Executar},
                 {"Interface - OO", Interface.Executar},
                 {"Sealed - OO", Sealed.Executar},
+
+                //Métodos & Funções
+                {"Exemplo Lambda - Métodos & Funções", ExemploLambda.Executar},
+                {"Lambdas Como Delegates - Métodos & Funções", LambdasDelegate.Executar},
+                {"Usando Delegates - Métodos & Funções", UsandoDelegates.Executar},
+                {"Delegates Com Função Anonima - Métodos & Funções", DelegateFunAnonima.Executar},
+                {"Delegates Como Parâmetros - Métodos & Funções", DelegatesComoParametros.Executar},
+                {"Métodos de Extensão - Métodos & Funções", MetodosDeExtensao.Executar},
             });
 
             central.SelecionarEExecutar();
